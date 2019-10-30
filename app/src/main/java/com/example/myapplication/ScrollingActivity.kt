bfa223a50d13acf8d32ff822c69cef362f0b3073
kmshack <kmshack@naver.com>
2019-10-30 14:02:49 +0900

    first commit

---
+++ app/src/main/java/com/example/myapplication/ScrollingActivity.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_scrolling.*
+
+class ScrollingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scrolling)
+
+        fab.setOnClickListener { view ->
+            app_bar.setExpanded(false, true)
+        }
+    }
+}
